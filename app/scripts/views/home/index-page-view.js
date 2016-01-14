321a4df08efc59fc68746551311edc66e2f1b7aa
caihua <caihua@corp.beisencorp.com>
2016-01-14 16:26:41 +0800

    zancun

--- app/scripts/views/home/index-page-view.js
+++ app/scripts/views/home/index-page-view.js
@@ -1,25 +1,40 @@
 define(['talent'
 	,'templates/home'
+	,'views/common/page-regions/header-view'
 ],function(Talent
 	,jst
+	,Header
 ) {
 	/**
 	 * Inner main view class
 	 * @class HomeView~MainView
 	 * @extends {Backbone.View}
 	 */	
+	var header = Talent.Model.extend({
+			defaults:{}
+	});
 	var MainView = Talent.Layout.extend(
 	{
 		template: jst['home/index-page']
 		,className: 'home-page-container'
-		,initialize: function() {}
+		,initialize: function() {
+			this.headerView = new Header({
+				model:new header()
+			});
+		}
 		,events:function(){
 			var events= {};
 			events["click .single-p"] = this.clkSP;
 			return events;
 		}
+		,regions: {
+			"header":".indexheader"
+		}
 		,onRender: function(){}
-		,onShow: function() {}
+		,onShow: function() {
+			this.header.show(this.headerView);
+
+		}
 		,clkSP:function(e){
 			Talent.app.request("apibox:getClassData").done(function(resp){
 				console.log(resp)
