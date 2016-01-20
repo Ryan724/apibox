3753f18f10aa5c7dc6039b2fe5802c8d6c9cc124
zhengpengjie <zhengpengjie@beisen.com>
2016-01-20 22:25:45 +0800

    l

--- tests/project-test.js
+++ tests/project-test.js
@@ -2,16 +2,16 @@ var assert = require("assert"),
     should = require("should"),
     Project = require('../lib/server/project');
 
-var pro = {"name": "UnitTest3", "desc": "单元测试3"};
+var pro ;
 
 describe('Project 基础功能测试', function() {
     it('可以新增一个项目', function() {
     	var timestamp=new Date().getTime();
-    	var pro1={"name":"UnitTest"+timestamp,"desc":"单元测试"+timestamp}
-    	var result=Project.add(pro1);
+    	var pro={"name":"UnitTest"+timestamp,"desc":"单元测试"+timestamp}
+    	var result=Project.add(pro);
     	
-		assert.equal(result.name,pro1.name);
-		assert.equal(result.desc,pro1.desc);
+		assert.equal(result.name,pro.name);
+		assert.equal(result.desc,pro.desc);
     	
         /*Project.add(pro).should.be.exactly(true);*/
     });
